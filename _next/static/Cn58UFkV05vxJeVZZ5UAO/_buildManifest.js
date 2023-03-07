2f9914634008b3f9966320c88b5a7c6e99cab4a0
JuanRMB <70106289@continental.edu.pe>
2023-03-07 12:47:50 -0500

    version 1

---
+++ _next/static/Cn58UFkV05vxJeVZZ5UAO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/855-f4acf9770a1de8f4.js","static/chunks/pages/index-89368bfec4b00685.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
